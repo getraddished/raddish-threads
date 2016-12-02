c132e7e4898610d30eb55a7575f9f89febfade9c
Jasper van Rijbroek <jasper@jvar.nl>
2016-12-02 21:27:34 +0100

    Moved to strict es6 classes.

--- index.js
+++ index.js
@@ -1,79 +1,79 @@
-/**
- * I have the idea of extending cluser for the ease of use and direct access to the classes.
- * This object will be a singleton for we only want to ever have a single object.
- */
+'use strict';
 
-var Promise = require('bluebird'),
-    usage = require('usage');
+var usage = require('usage');
 
-function RaddishThreads(config) {
-    this.cluster = require('cluster');
-    this.isMaster = this.cluster.isMaster;
-    this.isWorker = this.cluster.isWorker;
-    this.master = this.cluster;
-    this.config = config || {};
-    this.isWin = /^win/.test(process.platform);
-    this.threads = [];
-}
-
-RaddishThreads.prototype.getConfig = function() {
-    return {
-        interval: this.config['interval'] || 500,
-        maxThreads: this.config['maxThreads'] || 16,
-        minThreshold: this.config['minThreshold'] || 10,
-        maxThreshold: this.config['maxThreshold'] || 50
-    };
-};
-
-RaddishThreads.prototype.checkThreads = function() {
-    var promises = [],
-        self = this;
-
-    for(var index in this.threads) {
-        if(this.threads.hasOwnProperty(index)) {
-            var thread = this.threads[index];
-
-            promises.push(new Promise(function(resolve, reject) {
-                usage.lookup(thread.process.pid, function(err, result) {
-                    if(err) {
-                        return reject(err);
-                    }
-
-                    return resolve(result.cpu);
-                });
-            }));
-        }
+class RaddishThreads {
+    constructor() {
+        this.cluster = require('cluster');
+        this.isMaster = this.cluster.isMaster;
+        this.isWorker = this.cluster.isWorker;
+        this.master = this.cluster;
+        this.config = {};
+        this.isWin = /^win/.test(process.platform);
+        this.threads = [];
     }
 
-    Promise.all(promises)
-        .then(function(results) {
-            var config = self.getConfig(),
-                total = self.threads.length,
-                current = results.reduce(function(a, b) { return a + b; }, 0),
-                mean = current / total;
+    getConfig() {
+        return {
+            interval: this.config['interval'] || 500,
+            maxThreads: this.config['maxThreads'] || 16,
+            minThreshold: this.config['minThreshold'] || 10,
+            maxThreshold: this.config['maxThreshold'] || 50
+        };
+    }
 
-            if(mean > config.maxThreshold && total < config.maxThreads) {
-                console.log('Spawning process!');
-                threads.push(self.master.fork());
-            } else if(mean < config.minThreshold && total > 1) {
-                console.log('Killing process!');
-                threads.pop().kill();
+    checkThreads() {
+        var promises = [],
+            self = this;
+
+        for (var index in this.threads) {
+            if (this.threads.hasOwnProperty(index)) {
+                var thread = this.threads[index];
+
+                promises.push(new Promise(function (resolve, reject) {
+                    usage.lookup(thread.process.pid, function (err, result) {
+                        if (err) {
+                            return reject(err);
+                        }
+
+                        return resolve(result.cpu);
+                    });
+                }));
             }
-        });
-};
+        }
 
-RaddishThreads.prototype.start = function() {
-    if(this.isWin) {
-        console.log('Threads not suppoerted under Windows!');
-        return;
+        return Promise.all(promises)
+            .then(function (results) {
+                var config = self.getConfig(),
+                    total = self.threads.length,
+                    current = results.reduce(function (a, b) {
+                        return a + b;
+                    }, 0),
+                    mean = current / total;
+
+                if (mean > config.maxThreshold && total < config.maxThreads) {
+                    console.log('Spawning process!');
+                    this.threads.push(self.master.fork());
+                } else if (mean < config.minThreshold && total > 1) {
+                    console.log('Killing process!');
+                    this.threads.pop().kill();
+                }
+            });
     }
 
-    if(this.isMaster) {
-        console.log('Threads support enabled!');
-        this.threads.push(this.master.fork());
-    }
+    start() {
+        if(this.isWin) {
+            console.log('Threads not suppoerted under Windows!');
+            return;
+        }
 
-    setInterval(this.checkThreads.bind(this), this.getConfig().interval);
-};
+        if(this.isMaster) {
+            console.log('Threads support enabled!');
+            this.threads.push(this.master.fork());
+        }
+
+        setInterval(this.checkThreads.bind(this), this.getConfig().interval);
+    }
+}
 
 module.exports = RaddishThreads;
