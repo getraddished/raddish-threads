11a271e5d65ae9c9a41ede107bb12b130fd5b2f5
Jasper van Rijbroek <jasper@jvar.nl>
2017-04-22 19:18:35 +0200

    Added documentation.

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ var usage = require('usage'),
     cluster = require('cluster'),
     running = false;
 
+/**
+ * Allow for multithreading.
+ * The module supports two versions:
+ * 1. Allocate the threads wihout automatic scaling
+ * 2. Automatically scale the threads to remove threads when not in use.
+ *
+ * @class RaddishTreads
+ */
 class RaddishThreads {
     constructor(config) {
         this.config = Object.assign({
@@ -21,6 +29,17 @@ class RaddishThreads {
         this.bind();
     }
 
+    /**
+     * Run the bound method for the current thread.
+     *
+     * If the thread is a worker, then the worker method will be executed,
+     * however when there is no worker method, the master method is executed.
+     *
+     * If the current threads is the master threads the master method is called.
+     *
+     * @method init
+     * @returns {*}
+     */
     init() {
         if(typeof this.config[this._type] === 'function') {
             return this.config[this._type]();
@@ -29,6 +48,13 @@ class RaddishThreads {
         return this.config.master();
     }
 
+    /**
+     * Check which functionality to run and bind all the variables to it.
+     * If the scale option is enabled, override the default scale options with the options given (if any).
+     *
+     * @method bind
+     * @returns {boolean}
+     */
     bind() {
         if(this._type === 'worker') {
             return;
@@ -61,6 +87,13 @@ class RaddishThreads {
         return true;
     }
 
+    /**
+     * The scale method will automatically scale the threads if needed,
+     * also when a thread is not used anymore, it is killed.
+     *
+     * @method scale
+     * @param {Object} config The config object for the scale method.
+     */
     scale(config) {
         // We will now autospawn new threads.for (var thread of this.threads) {
         var promises = this.workers.map(function(worker) {
